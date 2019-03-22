383914cc44706521831c3bf5190da39a6bd1ed49
Christopher Carlson <ccarlson91@gmail.com>
2019-03-21 21:04:40 -0700

    more progress.

--- fortnite-app/app.js
+++ fortnite-app/app.js
@@ -2,18 +2,46 @@ const
     fortniteNodeModule = require('fortniteNodeModule'),
     inquirer = require('inquirer')
 
-
-async function getStats(gamerTag) {
-    const IDBody = await fortniteNodeModule.playerID(gamerTag);
-    const id = IDBody.uid;
-    const stats = await fortniteNodeModule.stats(id);
-    console.log(stats);
+ // Prompt to ask which game type. 
+ async function gameModePrompt(){
+    return inquirer.prompt([{ 
+        type: 'list',
+        name: 'searchCriteria',
+        message: `Which mode you like to see stats for \n `,
+        choices: ['solo','duo','squad']
+        }])
 }
-    
-//getStats("FugmaFanny")
 
+// Retrieve stats based on gamer tag. 
+async function getStatistics(gamerTag) {
+   
+    console.log("Hello gamer, welcome to my fortnite module!");
+    
+    // sets stats body to variable. 
+    const IDBody = await fortniteNodeModule.playerID(gamerTag);
+    const ID = IDBody.uid;
+    const statsBody = await fortniteNodeModule.stats(ID);
+
+    // Prompt to get game mode type results based of gamer tag.
+    const GameMode = await gameModePrompt();
+    
+            search = GameMode.searchCriteria;
+            let dataBody = '';
+
+            if(search === "solo"){
+                dataBody = statsBody.data.gamepad.defaultsolo;
+            }
+            else if(search === "duo"){
+                dataBody = statsBody.data.gamepad.defaultduo;
+            }
+            else if(search === "squad"){
+                dataBody = statsBody.data.gamepad.defaultsquad;
+            }
+            
+            console.log(`${gamerTag}: Kills -${dataBody.default.kills} Wins -${dataBody.default.placetop1} Matches Played -${dataBody.default.matchesplayed}` );
+}
 
 // Here are the function we right in this class, this sends them to client class. 
-// module.exports = {
-    
-// }
+module.exports = {
+    getStatistics
+}
--- fortnite-app/cli.js
+++ fortnite-app/cli.js
@@ -1,5 +1,20 @@
 const
-    yargs = require('yargs'),
+    yargs = require('yargs')
     app = require('./app')
 
-
+// usage typing so help command will work. 
+const flags = yargs
+    .usage('$0: Usage <cmd> [options]')
+    .command({
+        command: 'search',
+        desc: 'type in [search <username>] searches for stats based off a players epic account name.',
+        builder: (yargs) => {
+            return yargs.options('u', {
+                alias: 'username',
+                describe: "players epic acount name."
+            })
+        },
+        handler: (argv) => {app.getStatistics(argv.username)}
+    })
+    .help('help')
+    .argv
